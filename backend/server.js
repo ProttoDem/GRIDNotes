820e6a28075c9a2d018aba64db2963dfe228c4be
dmitrii nieliepin <dmitrii.nieliepin@formpipe.com>
2025-10-22 21:28:11 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -87,7 +87,7 @@ async function startServer() {
 			}
 		});
 
-		const PORT = process.env.PORT || 8080;
+		const PORT = 8080;
 		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 	} catch (err) {
 		console.error(err);
