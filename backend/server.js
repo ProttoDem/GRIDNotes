4da968524e7ef5145fc6befe8bd772b9e681fd98
dmitrii nieliepin <dmitrii.nieliepin@formpipe.com>
2025-10-22 21:22:35 +0300

    changed port for 8080

--- backend/server.js
+++ backend/server.js
@@ -87,7 +87,7 @@ async function startServer() {
 			}
 		});
 
-		const PORT = process.env.PORT || 3000;
+		const PORT = process.env.PORT || 8080;
 		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 	} catch (err) {
 		console.error(err);
